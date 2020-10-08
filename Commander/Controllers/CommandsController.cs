05c8daf4d6e098a32471fb0798fa3381fb627a96
fehepe <40445768+fehepe@users.noreply.github.com>
2020-10-07 22:28:48 -0400

    Controllers Implemented wit hard code variables

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,36 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+     
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commmands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        //GET api/commmands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+
+    }
+}
