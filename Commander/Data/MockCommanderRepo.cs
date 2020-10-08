0cea61d914362b5efe489f441fcd109a14036ae9
fehepe <40445768+fehepe@users.noreply.github.com>
2020-10-07 21:27:07 -0400

    Add project files.

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{ Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Platform = "Klette & Pan" },
+                new Command{ Id = 1, HowTo = "Cut bread", Line = "Get a Knife", Platform = "Knife & Chopping board" },
+                new Command{ Id = 2, HowTo = "Make a Cup of Tea", Line = "Place teabag in a Cup", Platform = "Klette & Cup" },
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Platform = "Klette & Pan" };
+        }
+    }
+}
